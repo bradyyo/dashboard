c694830af819e98353c30debc8fe75f2ac7ff9eb
He Yingxu <yingxu.he1998@gmail.com>
2018-02-12 17:39:34 +0800

    add src/containers/Full/Full.js

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -29,12 +29,12 @@ import * as firebase from 'firebase';
 // TODO: Replace with your project's customized code snippet
 
 var config = {
-    apiKey: "AIzaSyBPweAHcP4em0PYh8XWzEqjNhTk3OBNS5I",
-    authDomain: "dashboard-dev-8b843.firebaseapp.com",
-    databaseURL: "https://dashboard-dev-8b843.firebaseio.com",
-    projectId: "dashboard-dev-8b843",
-    storageBucket: "dashboard-dev-8b843.appspot.com",
-    messagingSenderId: "264557605189"
+    apiKey: "AIzaSyAe9N79Po3NYjL1dCebi4sHenl3Lhtcu-g",
+    authDomain: "dashboard-28479.firebaseapp.com",
+    databaseURL: "https://dashboard-28479.firebaseio.com",
+    projectId: "dashboard-28479",
+    storageBucket: "dashboard-28479.appspot.com",
+    messagingSenderId: "613043551242"
   };
 firebase.initializeApp(config);
 
